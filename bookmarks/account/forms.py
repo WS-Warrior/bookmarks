caf3290909a3c413198c30c80f68e29dfdc9f1ad
WS-Warrior <mei.py001@gmail.com>
2026-02-26 20:07:01 +0300

    регистрация, новые шаблоны

--- bookmarks/account/forms.py
+++ bookmarks/account/forms.py
@@ -14,3 +14,9 @@ class UserRegistrationForm(forms.ModelForm):
     class Meta:
         model = get_user_model() 
         fields = ['username', 'first_name', 'email']
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd['password2']:
+            raise forms.ValidationError("""Passwords don't match""")
+        return cd['password2']
