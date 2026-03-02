0614663b9f4a83b10241dbf2091f6de6eb730e1d
WS-Warrior <mei.py001@gmail.com>
2026-03-02 19:21:33 +0300

    создание букмарклета

---
+++ bookmarks/images/forms.py
@@ -0,0 +1,44 @@
+from django import forms
+from django.core.files.base import ContentFile
+from django.utils.text import slugify
+from .models import Images
+import requests
+
+
+class ImageCreateForm(forms.ModelForm):
+    class Meta:
+        model = Images
+        fields = ['title', 'url', 'description']
+        widgets = {'url': forms.HiddenInput}
+
+
+    def _clean_url(self):
+        url = self.cleaned_data['url']
+        valid_extensions = ['jpg', 'jpeg', 'png']
+        extention = url.rsplit('.', 1)[1].lower()
+
+        if extention not in valid_extensions:
+            raise forms.ValidationError('The given URL does not match valid image extentions.')
+        
+        return url
+    
+
+    def save(self, force_insert=False,
+             force_update=False,
+             commit=True):
+        image = super().save(commit=False)
+        image_url = self.cleaned_data['url']
+        name = slugify(image.title)
+        extention = image_url.rsplit('.', 1)[1].lower()
+        image_name = f'{name}.{extention}'
+
+        response = requests.get(image_url)
+        image.image.save(
+            image_name,
+            ContentFile(response.content),
+            save=False
+        )
+
+        if commit:
+            image.save()
+        return image
